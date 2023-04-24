2b200cb3fdea37311009facc229d569b39c0b1f1
Connor Vosberg <connorjv@gmail.com>
2023-04-24 17:01:02 -0600

    commit tree bf4b920ed8c528301369467772b7246f505ea4b2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves (dwarves_names)
+  dwarves_names.map.with_index(1) { |name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map { |call| "#{call.capitalize()}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+def find_the_cheese (foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # foods.any? do |food| 
+  #   if cheese_types.include?(food)
+  #     return food
+  #   else
+  #     nil
+  #   end
+  # end
+  # nil
+  foods.find do |food|
+    cheese_types.include?(food)
+  end
 end
+
